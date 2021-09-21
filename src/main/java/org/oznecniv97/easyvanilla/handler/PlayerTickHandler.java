9d80060e4fc0cf8fd3010eed4a1a9f514342967d
Vincenzo <vincenzo.salerno@live.it>
2021-09-21 02:21:56 +0200

    fix constant autofish mode for server use

--- src/main/java/org/oznecniv97/easyvanilla/handler/PlayerTickHandler.java
+++ src/main/java/org/oznecniv97/easyvanilla/handler/PlayerTickHandler.java
@@ -14,65 +14,104 @@ public class PlayerTickHandler {
 
     private static final Logger log = LogManager.getLogger();
 
+    private static final int MIN_BITING_COUNT = 4;
+    private static final int MIN_STOP_COUNT = 20;
+
+    private int bitingCount = 0;
+    private int stopCount = 0;
+    private boolean stopCheck = false;
+
     @SubscribeEvent
-    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+    public void onServerPlayerTick(TickEvent.PlayerTickEvent event) {
         //TODO: controllare se l'item si rompe, così da rimpiazzarlo, se presente
 //		event.player.getInventory().
 
         // controlla lo stato della canna da pesca a ogni tick del player
         final var hook = event.player.fishing;
         if (!stopCheck && hook != null) {
-            checkHookState(hook);
-        } else if(stopCheck && hook == null) {
-            log.info("stopcheck true after hooked");
-            stopCheck = false;
-            lastNibble = 0;
-        }
-    }
+            try {
+                //procedo al controllo solo se lo stato è bobbing, quindi galleggiante
+                var currentState = getStringField(hook, "currentState");
+                if (currentState.equals("BOBBING")) {
 
-    private int lastNibble = 0;
-    private boolean stopCheck = false;
-
-    private void checkHookState(FishingHook hook) {
-        try {
-            var currentState = getStringField(hook, "currentState");
-
-            if (currentState.equals("BOBBING")) {
-                var biting = getBooleanField(hook, "biting"); //se il pesce sta mordendo l'amo
-                var nibble = getIntField(hook, "nibble"); //tempo prima che il pesce vada via
-                var timeUntilLured = getIntField(hook, "timeUntilLured"); //tempo fino al prossimo arrivo del pesce
-                var timeUntilHooked = getIntField(hook, "timeUntilHooked"); //tempo fino al morso del pesce
-
-                if (biting) {
-                    log.info("event.player.fishing.biting: true");
-                }
-                if (nibble != 0) {
-                    log.info("event.player.fishing.nibble: {}", nibble);
-                }
-                if (timeUntilLured != 0) {
-                    log.info("event.player.fishing.timeUntilLured: {}", timeUntilLured);
-                }
-                if (timeUntilHooked != 0) {
-                    log.info("event.player.fishing.timeUntilHooked: {}", timeUntilHooked);
-                }
-
-                if (biting && nibble != 0) {
-                    log.info("biting && nibble!=0");
-                    if (lastNibble == 0) {
-                        log.info("lastNibble==0");
-                        lastNibble = nibble;
-                    } else if (lastNibble != nibble) {
-                        log.info("lastNibble!=nibble");
-                        stopCheck = true;
-                        lastNibble = 0;
+                    //controllo ricevo biting a true almeno MIN_BITING_COUNT volte
+                    var biting = getBooleanField(hook, "biting"); //se il pesce sta mordendo l'amo
+                    if(biting && ++bitingCount > MIN_BITING_COUNT) {
+                        //click tasto destro per pescare il pesce
                         KeyMapping.click(Minecraft.getInstance().options.keyUse.getKey());
+                        //attivo lo stop per attendere di aver ritirato l'hook
+                        stopCheck = true;
+                        //reset biting count
+                        bitingCount = 0;
                     }
                 }
+            } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+                log.error("Schiattata la reflection", e);
             }
-        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-            log.error("Schiattata la reflection", e);
+        } else if(stopCheck && hook==null && ++stopCount > MIN_STOP_COUNT) {
+            //click tasto destro per ricominciare a pescare
+            KeyMapping.click(Minecraft.getInstance().options.keyUse.getKey());
+            stopCheck = false;
+            stopCount = 0;
         }
     }
+
+//    private int lastNibble = 0;
+//    private boolean stopCheck = false;
+
+//    @SubscribeEvent
+//    public void onSinglePlayerTick(TickEvent.PlayerTickEvent event) {
+        //TODO: controllare se l'item si rompe, così da rimpiazzarlo, se presente
+//		event.player.getInventory().
+
+//        // controlla lo stato della canna da pesca a ogni tick del player
+//        final var hook = event.player.fishing;
+//        if (!stopCheck && hook != null) {
+//            try {
+//                var currentState = getStringField(hook, "currentState");
+//
+//                if (currentState.equals("BOBBING")) {
+//                    var biting = getBooleanField(hook, "biting"); //se il pesce sta mordendo l'amo
+//                    var nibble = getIntField(hook, "nibble"); //tempo prima che il pesce vada via
+//                    var timeUntilLured = getIntField(hook, "timeUntilLured"); //tempo fino al prossimo arrivo del pesce
+//                    var timeUntilHooked = getIntField(hook, "timeUntilHooked"); //tempo fino al morso del pesce
+//
+//                    if (biting) {
+//                        log.info("event.player.fishing.biting: true");
+//                    }
+//                    if (nibble != 0) {
+//                        log.info("event.player.fishing.nibble: {}", nibble);
+//                    }
+//                    if (timeUntilLured > 0) {
+//                        log.info("event.player.fishing.timeUntilLured: {}", timeUntilLured);
+//                    }
+//                    if (timeUntilHooked > 0) {
+//                        log.info("event.player.fishing.timeUntilHooked: {}", timeUntilHooked);
+//                    }
+//
+//                    if (biting && nibble != 0) {
+//                        log.info("biting && nibble!=0");
+//                        if (lastNibble == 0) {
+//                            log.info("lastNibble==0");
+//                            lastNibble = nibble;
+//                        } else if (lastNibble != nibble) {
+//                            log.info("lastNibble!=nibble");
+//                            stopCheck = true;
+//                            lastNibble = 0;
+//                            KeyMapping.click(Minecraft.getInstance().options.keyUse.getKey());
+//                        }
+//                    }
+//                }
+//            } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+//                log.error("Schiattata la reflection", e);
+//            }
+//        } else if(stopCheck && hook == null) {
+//            log.info("stopcheck true after hooked");
+//            stopCheck = false;
+//            lastNibble = 0;
+//            KeyMapping.click(Minecraft.getInstance().options.keyUse.getKey());
+//        }
+//    }
 
     private String getStringField(Object obj, String fieldName) throws NoSuchFieldException, IllegalAccessException {
         return getField(obj, fieldName).get(obj).toString();
